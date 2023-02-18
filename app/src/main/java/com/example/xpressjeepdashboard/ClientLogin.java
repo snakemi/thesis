fd576589a329418819fb0a85a2b73182c0b74c04
snakemi <35140132+snakemi@users.noreply.github.com>
2023-02-19 02:27:27 +0800

    Implement Firebase and Login authentication on the Client/Driver logins

--- app/src/main/java/com/example/xpressjeepdashboard/ClientLogin.java
+++ app/src/main/java/com/example/xpressjeepdashboard/ClientLogin.java
@@ -5,25 +5,61 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ClientLogin extends Activity {
+    private FirebaseAuth mAuth;
+    private EditText EmailAddress, Password;
     private Button clientLoginB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.client_login);
+        mAuth = FirebaseAuth.getInstance();
+        EmailAddress = findViewById(R.id.editTextTextEmailAddress);
+        Password = findViewById(R.id.editTextTextPassword);
+
 
         clientLoginB = findViewById(R.id.button4);
 
         clientLoginB.setOnClickListener(new View.OnClickListener(){
 
             @Override
+            //public void onClick(View view) {
+//                openViewMap();
+//            }
             public void onClick(View view) {
-                openViewMap();
+                login();
             }
         });
     }
+
+    private void login() {
+        String user = EmailAddress.getText().toString().trim();
+        String pass = Password.getText().toString().trim();
+        mAuth.signInWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(ClientLogin.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    openViewMap();
+                }
+                else{
+                    Toast.makeText(ClientLogin.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
     public void openViewMap(){
         Intent intent = new Intent(this, ViewMap.class);
         startActivity(intent);
--- app/src/main/java/com/example/xpressjeepdashboard/DriverLogin.java
+++ app/src/main/java/com/example/xpressjeepdashboard/DriverLogin.java
@@ -5,8 +5,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DriverLogin extends Activity {
+    private FirebaseAuth mAuth;
+    private EditText EmailAddress, Password;
     private Button driverLoginB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,17 +25,40 @@ public class DriverLogin extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.driver_login);
-
+        mAuth = FirebaseAuth.getInstance();
+        EmailAddress = findViewById(R.id.editTextTextEmailAddress);
+        Password = findViewById(R.id.editTextTextPassword);
         driverLoginB = findViewById(R.id.button4);
 
         driverLoginB.setOnClickListener(new View.OnClickListener(){
 
             @Override
+            //public void onClick(View view) {
+            //    openJeepInfo();
+            //}
             public void onClick(View view) {
-                openJeepInfo();
+                login();
             }
         });
     }
+
+    private void login() {
+        String user = EmailAddress.getText().toString().trim();
+        String pass = Password.getText().toString().trim();
+        mAuth.signInWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(DriverLogin.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    openJeepInfo();
+                }
+                else{
+                    Toast.makeText(DriverLogin.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
     public void openJeepInfo(){
         Intent intent = new Intent(this, JeepInfo.class);
         startActivity(intent);
