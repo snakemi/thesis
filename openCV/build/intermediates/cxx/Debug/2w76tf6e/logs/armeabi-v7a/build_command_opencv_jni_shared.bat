e0570723d60c1ef4a0b8430f65a5ac710d3bd7cf
snakemi <35140132+snakemi@users.noreply.github.com>
2023-04-01 16:20:01 +0800

    actually delete opencv this time

--- openCV/build/intermediates/cxx/Debug/2w76tf6e/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\ACERNitro5\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\ACERNitro5\\StudioProjects\\thesis\\openCV\\.cxx\\Debug\\2w76tf6e\\armeabi-v7a" ^
-  opencv_jni_shared
